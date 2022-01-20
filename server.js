b929be14eff143ce40c79c7876486b18edb240b2
lomantic <77719867+lomantic@users.noreply.github.com>
2022-01-20 17:45:49 +0900

    ..

---
+++ server.js
@@ -0,0 +1,48 @@
+import { PrismaClient } from "@prisma/client";
+import {ApolloServer, gql} from "apollo-server";
+
+const client = new PrismaClient();
+
+const typeDefs = gql`
+
+    type Movie{
+        id: Int!
+        title: String!
+        year: Int!
+        genre: String
+        createdAt: String!
+        upadatedAt: String!
+    }
+
+    type Query {
+        movies: [Movie]
+        movie: Movie
+    }
+    type Mutation {
+        createMovie(title: String!, year: Int!, genre: String) : Movie
+        deleteMovie(title: String!): Boolean
+    }
+`;
+
+const resolvers = {
+    Query: {
+        movies: () => client.movie.findMany(),
+        movie: () => ({ title: "Wow", year: 14 }),
+    },
+    Mutation: {
+        createMovie: (_,{ title, year, genre }) => (
+            client.movie.create({ data : {
+                title,
+                year,
+                genre,
+            }})
+        ),
+    }
+};
+
+const server = new ApolloServer({
+    typeDefs, 
+    resolvers
+});
+
+server.listen().then(() => console.log("Apollo Server open http://localhost:4000/"))
