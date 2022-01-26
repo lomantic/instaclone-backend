3e7a54e66d02aa2e007fbffb412643e1b31be952
lomantic <77719867+lomantic@users.noreply.github.com>
2022-01-26 18:52:38 +0900

    token

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,4 +1,5 @@
 import client from "../client";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 export default {
   Mutation: {
@@ -6,7 +7,7 @@ export default {
       _,
       { firstName, lastName, username, email, password }
     ) => {
-      //check if username or email is unique on DB
+      // check if username or email is unique on DB
       try {
         const existingUser = await client.user.findFirst({
           where: {
@@ -41,6 +42,30 @@ export default {
       } catch (e) {
         return e;
       }
+    },
+    login: async (_, { username, password }) => {
+      // find user with args.username
+      const user = await client.user.findFirst({ where: { username } });
+      if (!user) {
+        return {
+          ok: false,
+          error: "User not found",
+        };
+      }
+      // check pwd with args.pwd
+      const passwordOk = await bcrypt.compare(password, user.password);
+      if (!passwordOk) {
+        return {
+          ok: false,
+          error: "Incorrect password",
+        };
+      }
+      // issue a token and send it to user
+      const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+      return {
+        ok: true,
+        token,
+      };
     },
   },
 };
